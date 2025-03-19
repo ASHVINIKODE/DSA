8cca13acd5eae0fd29394537a0374844a9a5aa7b
ASHVINIKODE <155357703+ASHVINIKODE@users.noreply.github.com>
2025-03-19 22:02:27 +0530

    initial commit

---
+++ CountOccurence.java
@@ -0,0 +1,22 @@
+public class CountOccurence {
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 3, 4, 5, 6, 1, 1, 1, 3, 4, 5, 3, 4, 5, 9, 8, 9 };
+        int target = 1;
+        int count = countOccurence(arr, target);
+        System.out.println("Target appears " + count + " times");
+    }
+
+    static int countOccurence(int[] arr, int target) {
+        if (arr.length == 0) {
+            System.out.println("Array is empty");
+            return 0;
+        }
+        int count = 0;
+        for (int num : arr) {
+            if (num == target) {
+                count++;
+            }
+        }
+        return count;
+    }
+}

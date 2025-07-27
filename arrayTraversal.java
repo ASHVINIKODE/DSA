ab7bfcbea6777df39a7d1df644568f252a5eae80
ASHVINIKODE <155357703+ASHVINIKODE@users.noreply.github.com>
2025-07-27 22:51:02 +0530

    initial commit

---
+++ arrayTraversal.java
@@ -0,0 +1,9 @@
+public class arrayTraversal {
+    public static void main(String[] args) {
+        int[] arr = { 2, 3, 4, 5, 6, 7 };
+        System.out.println("Array elements are:");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println("Element at index " + i + "=" + arr[i]);
+        }
+    }
+}

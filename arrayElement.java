0dc6703f5c0d791146d8b887b2ebfdda7288d22d
ASHVINIKODE <155357703+ASHVINIKODE@users.noreply.github.com>
2025-07-27 23:44:43 +0530

    initial commit

---
+++ arrayElement.java
@@ -0,0 +1,9 @@
+public class arrayElement {
+    public static void main(String[] args) {
+        int[] arr = { 2, 3, 4, 5, 6, 7, 8, 9 };
+        System.out.println("All element of an array:");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
